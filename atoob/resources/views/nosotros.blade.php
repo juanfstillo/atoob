f2a15b6ef78cffeca86cbaab0bd4bbc4f1140ac2
juanfstillo <juan.stillo1@gmail.com>
2020-03-11 19:30:23 -0300

    vista nosotros

---
+++ atoob/resources/views/nosotros.blade.php
@@ -0,0 +1,37 @@
+@extends('layout')
+@section('title')
+  ATOOB -Nosotros-
+@endsection
+
+@section('main')
+  <div class="card m-5" >
+    <div class="card-body">
+      <p class="text-justify">Un sábado habiendo finalizado de hacer las cirugías, pasé por el círculo médico para informarme como siempre, los códigos de las prácticas realizadas y las empleadas, me comentaron lo que había ocurrido en una ciudad vecina, en donde un cirujano traumatólogo habría discutido verbal y de una manera muy violenta con un anestesista, pues la cirugía que iban a realizar, solo podría cobrar honorarios el anestesista al  tener convenio con dicho seguro laboral.- Cuestión que la situación incomodó al cirujano, quién se sintió ciertamente desamparado.
+
+    Todo ese fin de semana estuve pensando seriamente en lo ocurrido, pues esas prácticas, si bien por mi lado, podría cobrarlas simplemente por ser dueño de la clínica, había muchísimos colegas que no lo podrían hacer, simplemente por estar dentro de un sistema, en donde los Seguros hacían perversamente acuerdos con las clínicas, dejando a los cirujanos de lado, los que luego deberían  suplicar la remuneración de su trabajo sin saber cuando ni cuanto lo conseguirían.
+
+    Estaba a la vista que todas aquellas instituciones que deberían defender al sector profesional médico, no solo vivían de nosotros, sino que siempre implementaban actos de denigración al trabajo médico.- Mis sentimientos y pensamientos fueron acorde a la realidad, el desempeño profesional había llegado a la total falta de dignidad.-
+
+    Las instituciones que nos deberían cuidar habían tomado un rumbo equivocado, así como se creo para tal fín el Galeno Móvil, pago por Cápitas, pago por Módulos, en fin todas aquellas medidas que destrozaban los honorarios médicos, por ende al médico, haciéndolo partícipe solo de las perdidas siendo la única causa:  alimentar a las instituciones gerenciadoras que viven gracias a nosotros.-
+
+    Fue así que en el año 1998, tuve la osadía de llamar por teléfono a los colegas de las ciudades de Bragado  y de Nueve de Julio, informándoles que me encontraba en reunión con los colegas de la ciudad de Chivilcoy y les ofrecí hacer algún tipo de asociación para tratar de mejorar nuestra dignidad.- A la inversa llamé a los colegas de la ciudad de Chivilcoy y les propuse lo mismo.- Claro esta que no me había reunido con nadie, solo ni intención fue poner una fecha de encuentro en mi ciudad de Alberti dentro de los 15 días próximos.-
+
+    Fue de esa manera que nos reunimos en mi ciudad.- Solo eramos ocho profesionales.- Ellos pensaban  de la misma manera y gracias a ellos, salí a recorrer todas las ciudades vecinas: Junin, Pehuajo, Trenque Lauquen, Mercedes, Pergamino, Arrecifes, Olavarría , Chacabuco y otras.-
+
+    Si bien estaba muy motivado, me encontraba con profesionales abúlicos, desganados y que de alguna manera por no menos de 24 hs, me hacían caer en un estado de impotencia y depresión transitoria.-  Fuí instruido y ayudado legalmente por un abogado íntimo amigo Dr Carlos A de Irureta, quien gracias a él tramité la personería jurídica y la realización del estatuto.-
+
+    Nuestra asociación se llamó ATOOB (Asociación de Traumatología y Ortopedia del oeste bonaerense).- En ese entonces teníamos diez socios sin cuota alguna, pero sí: nombre y apellido de nuestra asociación: ATOOB.-
+
+    Los viajes a Capital Federal fueron interminables.- Iba y volvía sin conseguir ningún convenio.- No obstante, confieso haber pasado por momentos difíciles de superar pero nunca dejé de mirar aquel horizonte casi imposible de llegar.-
+
+    En el año 2001 gracias a dos médicos, uno de Consolidar y otro de Mapfre Argentina me conceden dos acuerdos laborales para nuestra organización.- Recuerdo que ese día volé tan alto que sinceramente tuve sentimientos de fundador.-
+
+    Los años fueron transcurriendo y actualmente somos centenares de profesionales que seguimos defendiendo lo mismo: Nuestra dignidad y para tal fin, estamos UNIDOS en la ATOOB, esta organización que tiene muchísimos acuerdos laborales y con el propósito de seguir creciendo.-
+  </p>
+    <br>
+    <p class='text-center'>Nuestro lema es: TODOS UNIDOS PODEMOS DEFENDER LA DIGNIDAD MEDICA</p>
+   </div>
+  </div>
+
+
+@endsection
